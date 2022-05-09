cecb5684d70ebdbd9e02369a6aba1ba71de61eba
zigfridExe <zigfrid.exe@gmail.com>
2022-05-09 17:47:26 -0300

    Integração com o insomnia usando HTML no server Ts

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,16 @@ const app = express();
 
 app.use(express.json()); //Midleware o tal do middleware
 
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "2f0e519cbb42a7",
+        pass: "34136faa34638f"
+    }
+});
+
+
 app.post('/feedbacks', async (req, res) => {
     const { type, coment, screenshot } = req.body;
 
@@ -16,6 +26,18 @@ app.post('/feedbacks', async (req, res) => {
             screenshot,
         }
     })
+
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi@fidget.com>',
+        to: 'Everton Lyons Romansini<zigfrid.exe@gmail.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentario: ${coment}</p>`,
+            `</div>`,
+        ].join('')
+    });
 
     return res.status(201).json({ data: feedback });
 })
